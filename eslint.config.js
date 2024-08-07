380992b4366aba9f8812a67cfb38d73be73eebac
palmdrop <anton@exlex.se>
2024-08-07 15:57:54 +0200

    setup eslint and prettier for linting/formatting

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+/** @type { import("eslint").Linter.Config[] } */
+export default [
+  // Config files (node)
+  {
+    files: ['view/config/**/*.js'],
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+    languageOptions: {
+      globals: globals.node,
+    },
+    plugins: {
+      format: prettierPlugin,
+    },
+  },
+  // Browser logic files (browser)
+  /*
+  {
+    files: ["TODO"],
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+    languageOptions: { 
+      globals: globals.browser
+    },
+  },
+  */
+  eslintConfigPrettier,
+];
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+export default {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+};
